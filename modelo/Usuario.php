a081963da5fe147bfedbc7c5d54f2fd783dcb71c
jhonatan <jhonaccoyso@gmail.com>
2021-04-29 16:49:41 -0500

    agregue funcion editar y cambiar contraseña

--- modelo/Usuario.php
+++ modelo/Usuario.php
@@ -40,5 +40,34 @@ class Usuario{
             echo 'noupdate';
         }
     }
+    function cambiar_photo($id_usuario,$nombre){
+        $sql="SELECT avatar FROM usuario where id_usuario=:id";
+        $query = $this->acceso->prepare($sql);    
+        $query->execute(array(':id'=>$id_usuario));
+        $this->objetos = $query->fetchall();
+
+            $sql="UPDATE usuario SET avatar=:nombre where id_usuario=:id";
+            $query=$this->acceso->prepare($sql);
+            $query->execute(array(':id'=>$id_usuario,':nombre'=>$nombre));
+          return $this->objetos;
+        
+    }
+    function buscar(){
+        if(!empty($_POST['consulta'])){
+            $consulta=$_POST['consulta'];
+            $sql="SELECT * FROM usuario join tipo_us on us_tipo=id_tipo_us where nombre_us LIKE :consulta";
+            $query = $this->acceso->prepare($sql);
+            $query->execute(array(':consulta'=>"%$consulta%"));
+            $this->objetos=$query->fetchall();
+            return $this->objetos;
+        }
+        else{
+            $sql="SELECT * FROM usuario join tipo_us on us_tipo=id_tipo_us where nombre_us NOT LIKE '' ORDER BY id_usuario LIMIT 25";
+            $query = $this->acceso->prepare($sql);
+            $query->execute();
+            $this->objetos=$query->fetchall();
+            return $this->objetos;
+        }
+    }
 }
 ?>
