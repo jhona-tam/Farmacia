6d90600f3c060c1213aab87fcb2397f1e637fd0a
jhonatan <jhonaccoyso@gmail.com>
2021-04-29 16:47:02 -0500

    agregue funcion de agregar foto y buscar usuarios

--- controlador/UsuarioController.php
+++ controlador/UsuarioController.php
@@ -1,6 +1,8 @@
 <?php
 include_once '../modelo/Usuario.php';
 $usuario = new Usuario();
+session_start();
+$id_usuario= $_SESSION['usuario'];
 if($_POST['funcion']=='buscar_usuario'){
     $json=array();
     $fecha_actual = new DateTime();
@@ -19,7 +21,9 @@ if($_POST['funcion']=='buscar_usuario'){
             'residencia'=>$objeto->residencia_us,
             'correo'=>$objeto->correo_us,
             'sexo'=>$objeto->sexo_us,
-            'adicional'=>$objeto->adicional_us
+            'adicional'=>$objeto->adicional_us,
+            'avatar'=>'../img/'.$objeto->avatar
+
         );
     }
     $jsonstring = json_encode($json[0]);
@@ -56,5 +60,57 @@ if($_POST['funcion']=='cambiar_contra'){
     $oldpass=$_POST['oldpass'];
     $newpass=$_POST['newpass'];
     $usuario->cambiar_contra($id_usuario,$oldpass,$newpass);
+}
+if($_POST['funcion']=='cambiar_foto'){
+    if(($_FILES['photo']['type']=='image/jpeg')||($_FILES['photo']['type']=='image/png')||($_FILES['photo']['type']=='image/gif')){
+        $nombre=uniqid().'-'.$_FILES['photo']['name'];
+        $ruta='../img/'.$nombre;
+        move_uploaded_file($_FILES['photo']['tmp_name'],$ruta);
+        $usuario->cambiar_photo($id_usuario,$nombre);
+        foreach ($usuario->objetos as $objeto) {
+            unlink('../img/'.$objeto->avatar);
+        }
+        $json= array();
+        $json[]=array(
+            'ruta'=>$ruta,
+            'alert'=>'edit'
+        );
+        $jsonstring = json_encode($json[0]);
+        echo $jsonstring;
+    }
+    else{
+        $json= array();
+        $json[]=array(
+            'alert'=>'noedit'
+        );
+        $jsonstring = json_encode($json[0]);
+        echo $jsonstring;
+    }
+}
+if($_POST['funcion']=='buscar_usuarios_adm'){
+    $json=array();
+    $fecha_actual = new DateTime();
+    $usuario->buscar();
+    foreach ($usuario->objetos as $objeto) {
+        $nacimiento = new DateTime($objeto->edad);
+        $edad = $nacimiento->diff($fecha_actual);
+        $edad_years = $edad->y;
+        $json[]=array(
+            'nombre'=>$objeto->nombre_us,
+            'apellidos'=>$objeto->apellidos_us,
+            'edad'=>$edad_years,
+            'dni'=>$objeto->dni_us,
+            'tipo'=>$objeto->nombre_tipo,
+            'telefono'=>$objeto->telefono_us,
+            'residencia'=>$objeto->residencia_us,
+            'correo'=>$objeto->correo_us,
+            'sexo'=>$objeto->sexo_us,
+            'adicional'=>$objeto->adicional_us,
+            'avatar'=>'../img/'.$objeto->avatar
+
+        );
+    }
+    $jsonstring = json_encode($json);
+    echo $jsonstring;
 }
 ?>
