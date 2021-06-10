e4a5f630638454e7ded3700276c8f784d9d1492d
jhonatan <jhonaccoyso@gmail.com>
2021-06-10 17:03:41 -0500

    Hices que en la vista de catalogo se vea los productos de venta y tambien en la parte superior una tabla que indique con color los lotes vencidos o por vencer #23

--- vista/adm_catalogo.php
+++ vista/adm_catalogo.php
@@ -15,46 +15,71 @@ include_once 'layouts/nav.php'
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Titulo del contenido</h1>
+            <h1>Cátalogo</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Black Page</li>
+              <li class="breadcrumb-item active">Cátalogo</li>
             </ol>
           </div>
         </div>
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- Main content -->
-    <section class="content">
-      <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Title</h3>
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
-        </div>
-        <div class="card-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          Footer
-        </div>
-        <!-- /.card-footer-->
       </div>
-      <!-- /.card -->
     </section>
-    <!-- /.content -->
+    <!--lotes que se muestran por fecha de vencimiento-->
+    <section>
+        <div class="container-fluid">
+            <div class="card card-danger">
+                <div class="card-header">
+                    <h3 class="card-title">Lotes en riesgo</h3>                    
+                </div>
+                <div class="card-body p-0 table-responsive">
+                  <table class="table table-hover text-nowrap">
+                    <thead class="table-success">
+                      <tr>
+                        <th>Cod</th>
+                        <th>Producto</th>
+                        <th>Stock</th>
+                        <th>Laboratorio</th>
+                        <th>Presentacion</th>
+                        <th>Proveedor</th>
+                        <th>Mes</th>
+                        <th>Dia</th>
+                      </tr>
+                    </thead>                    
+                    <tbody id="lotes" class="table-active">
+                    </tbody>
+                  </table>                  
+                </div>
+                <div class="card-footer">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--contenido del catalogo de productos-->
+    <section>
+        <div class="container-fluid">
+            <div class="card card-success">
+                <div class="card-header">
+                    <h3 class="card-title">Buscar producto</h3>
+                    <div class="input-group">
+                        <input type="text" id="buscar-producto" class="form-control float-left" placeholder="Ingrese nombre de producto">
+                        <div class="input-group-append">
+                        <button class="btn btn-default"><i class="fas fa-search"></i></button></div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div id="productos" class="row d-flex aling-items-stretch">
+
+                    </div>
+                </div>
+                <div class="card-footer">
+
+                </div>
+            </div>
+        </div>
+    </section>
   </div>
-  <!-- /.content-wrapper -->
 <?php
 include_once 'layouts/footer.php';
 }
@@ -62,3 +87,4 @@ else{
     header('Location: ../index.php');
 }
 ?>
+<script src="../js/Catalogo.js"></script>
