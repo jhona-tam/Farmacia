14d4efea57564c727341da32358db4ede3799f20
jhonatan <jhonaccoyso@gmail.com>
2021-06-11 17:16:01 -0500

    hice que los productos añadidos al carrito se eliminen pero hacindo uso de localstorage que cuando actualizemos no se elimine siempre en cuando usemos el boton de vaciar carrito o el icono de eliminar #26

--- js/Carrito.js
+++ js/Carrito.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
+RecuperarLS_carrito();
 
+	/**funcion agregar carrito */
     $(document).on('click','.agregar-carrito',(e)=>{
         const elemento = $(this)[0].activeElement.parentElement.parentElement.parentElement.parentElement;
         const id = $(elemento).attr('prodId');
@@ -24,8 +26,73 @@ $(document).ready(function(){
             avatar: avatar,
             cantidad:1
         }
-        template=`
-            <tr>
+				let id_producto;
+				let productos;
+				productos = RecuperarLS();
+				productos.forEach(prod => {
+					if(prod.id===producto.id){
+						id_producto=prod.id;
+					}
+				});
+				if(id_producto === producto.id){
+					Swal.fire({
+						icon: 'error',
+						title: 'Oops...',
+						text: 'El producto se repite!',
+					})
+				}
+				else{
+					template=`
+					<tr prodId="${producto.id}">
+							<td>${producto.id}</td>
+							<td>${producto.nombre}</td>
+							<td>${producto.concentracion}</td>
+							<td>${producto.adicional}</td>
+							<td>${producto.precio}</td>
+							<td><button class="borrar-producto btn btn-danger"><i class="fas fa-times-circle"></i></button></td>
+					</tr>
+			`;
+			$('#lista').append(template);
+			AgregarLS(producto);
+			}        
+    })
+		/**borrar prodcuto de carrito */
+    $(document).on('click','.borrar-producto',(e)=>{
+        const elemento = $(this)[0].activeElement.parentElement.parentElement;
+				const id = $(elemento).attr('prodId');
+        elemento.remove();
+				Eliminar_producto_LS(id);
+    })
+		/**vaciar carrito */
+    $(document).on('click','#vaciar-carrito',(e)=>{
+        $('#lista').empty();
+				EliminarLS();
+    })
+		/**funcion de recuperar con local storage los productos cuando actualizamos */
+		function RecuperarLS() {
+			let productos;
+			if (localStorage.getItem('productos')===null){
+				productos=[];
+			}
+			else{
+				productos= JSON.parse(localStorage.getItem('productos'))
+			}
+			return productos
+		}
+		/**funcion de agregar con local storage productos */
+		function AgregarLS(producto){
+			let productos;
+			productos = RecuperarLS();
+			productos.push(producto);
+			localStorage.setItem('productos',JSON.stringify(productos))
+		}
+		/**funcion de recuperar carrito con local storage */
+		function RecuperarLS_carrito(){
+			let productos;
+			productos = RecuperarLS();
+			productos.forEach(producto => {
+				template=`
+            <tr prodId="${producto.id}">
                 <td>${producto.id}</td>
                 <td>${producto.nombre}</td>
                 <td>${producto.concentracion}</td>
@@ -35,12 +102,21 @@ $(document).ready(function(){
             </tr>
         `;
         $('#lista').append(template);
-    })
-    $(document).on('click','.borrar-producto',(e)=>{
-        const elemento = $(this)[0].activeElement.parentElement.parentElement;
-        elemento.remove();        
-    })
-    $(document).on('click','#vaciar-carrito',(e)=>{
-        $('#lista').empty();
-    })
+			});
+		}
+		/**eliminar productos de local storage con el boton */
+		function Eliminar_producto_LS(id){
+			let productos;
+			productos = RecuperarLS();
+			productos.forEach(function(producto,indice) {
+				if (producto.id===id){
+					productos.splice(indice,1);
+				}
+			});
+			localStorage.setItem('productos',JSON.stringify(productos));
+		}
+		/**elimanar local storage */
+		function EliminarLS(){
+			localStorage.clear();
+		}
 })
